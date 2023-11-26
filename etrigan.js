c6929b0d7f38e76145ba164b13591f4d47d399e3
hariv <hvenugopalan@ucdavis.edu>
2023-11-25 19:29:29 -0800

    handle injection error

--- etrigan.js
+++ etrigan.js
@@ -1,9 +1,11 @@
-var codeToInject = 'Object.defineProperty(navigator,"platform", { \
-  get: function () { return "EtriganPlatform"; }, \
-  set: function (a) {} \
- });';
+var codeToInject = 'Object.defineProperty(navigator, "platform", {value: "EtriganPlatform"; });';
+//var codeToInject = 'Object.defineProperty(navigator,"platform", { \
+//  get: function () { return "EtriganPlatform"; }, \
+//  set: function (a) {} \
+// });';
 
 var script = document.createElement('script');
-script.appendChild(document.createTextNode(codeToInject));
+script.setAttribute('type', 'text/javascript');
+script.innerHTML = codeToInject;
 (document.head || document.documentElement).appendChild(script);
 script.parentNode.removeChild(script);

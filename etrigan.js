8acaaa13c61e72667a597f5ae575ec590389e11d
Mona Wang <86867201+monawang09@users.noreply.github.com>
2023-12-02 07:44:51 +0800

    Update etrigan.js
    Yes I manually did this commit

--- etrigan.js
+++ etrigan.js
@@ -1,10 +1,32 @@
-var featureIndex = globalThis.featureMap["plugins"]; // Change to desired feature
-
-var etrigan = globalThis.config[featureIndex].code;
-etrigan = etrigan.replace("etrigan_placeholder", globalThis.config[featureIndex].value);
-
+let featureIndex = 0;
+let etrigan = '';
+let spoof = ["deviceMemory", "hardwareConcurrency", "languages", "platform", "timezone","touchSupportMaxTouchPoints", "touchSupportTouchStart", "touchSupp
+portTouchEvent", "fontPreferencesSystem", "webdriver"]
+for (let i = 0; i < spoof.length; i++){
+    let feature = spoof[i];
+    console.log(`Spoofing Feature: ${feature}`);
+    featureIndex = globalThis.featureMap[spoof[i]];
+    let tmp = globalThis.config[featureIndex].code
+    if (feature ==  "deviceMemory") {
+        tmp = tmp.replace("etrigan_placeholder", 2);
+    } else if (feature == "languages") {
+        tmp = tmp.replace("etrigan_placeholder", "zh-CN");
+    } else if (feature == "platform") {
+        tmp = tmp.replace("etrigan_placeholder", "Win32");
+    } else if (feature == "timezone"){
+        tmp = tmp.replace("etrigan_placeholder", "Asia/Shanghai");
+    } else if (feature = "touchSupportMaxTouchPoints") {
+        tmp = tmp.replace("etrigan_placeholder", [4]);
+     } else if (feature == "fontPreferencesSystem") {
+             tmp = tmp.replace("etrigan_placeholder", 161.296875);
+     }else {
+        tmp = tmp.replace("etrigan_placeholder", globalThis.config[featureIndex].value);
+    }     
+  etrigan += tmp;
+}
+// Script injection 
 var script = document.createElement('script');
 script.setAttribute('type', 'text/javascript');
 script.innerHTML = etrigan;
 (document.head || document.documentElement).appendChild(script);
-//script.parentNode.removeChild(script);
+script.parentNode.removeChild(script);
